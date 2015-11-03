1b27f7905f095d4a3d7a31dd3ba9189cac731acf
Jeremy Raines <jraines@gmail.com>
2015-11-03 09:30:29 -0500

    Show message in console on hot reload

--- src/simoutfit/core.cljs
+++ src/simoutfit/core.cljs
@@ -7,10 +7,11 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world!!!"}))
 
 
 (defn on-js-reload []
+  (println "Reloaded!")
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
